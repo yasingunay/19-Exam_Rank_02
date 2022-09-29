d9f36d6769a7a4e59d1c84826fe0bfd8df408916
yasingunay <chalouper@icloud.com>
2022-09-29 18:00:39 +0200

    up

--- union.c
+++ union.c
@@ -5,60 +5,62 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ygunay <ygunay@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/29 15:37:58 by ygunay            #+#    #+#             */
-/*   Updated: 2022/09/29 15:38:19 by ygunay           ###   ########.fr       */
+/*   Created: 2022/09/29 18:00:01 by ygunay            #+#    #+#             */
+/*   Updated: 2022/09/29 18:00:05 by ygunay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	check_repeat(char *str, char c, int n)
+int check_repeat(char *str, char c, int n)
 {
-	int	i;
-
+	int i;
+	
 	i = 0;
-	while (i < n)
+
+	while(i < n)
 	{
-		if (str[i] == c)
+		if(str[i]==c)
 			return (0);
 		i++;
 	}
-	return (1);
+	return(1);
 }
 
-void	ft_strjoin(char *s1, char *s2)
+void ft_strjoin(char *s1, char *s2)
 {
-	int	i;
-	int	j;
+	int i;
+	int j;
 
-	i = 0;
+	i =0;
+	while(s1[i])
+		i++;
 	j = 0;
-	while (s1[i])
-		i++;
-	while (s2[j])
-	{
-		s1[i] = s2[j];
-		i++;
-		j++;
-	}
-	s1[i] = '\0';
+	while(s2[j])
+		{
+			s1[i]=s2[j];
+			i++;
+			j++;
+		}
+	s1[i]='\0';
 }
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
-	int	i;
+	int i;
 
 	i = 0;
-	if (ac == 3)
+	if(ac == 3)
 	{
-		ft_strjoin(av[1], av[2]);
-		while (av[1][i])
+		ft_strjoin(av[1],av[2]);
+		while(av[1][i])
 		{
-			if (check_repeat(av[1], av[1][i], i))
-				write (1, &av[1][i], 1);
+			if(check_repeat(av[1],av[1][i],i))
+			write(1, &av[1][i],1);
 			i++;
 		}
 	}
-	write (1, "\n", 1);
-	return (0);
+	write(1,"\n",1);
+
+	return 0;
 }
